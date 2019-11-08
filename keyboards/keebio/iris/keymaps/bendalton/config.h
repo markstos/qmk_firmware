783a414d2efafeaf7eb5a3133e3b4a0418aadc13
Ben Dalton <bendalton@rideamigos.com>
2019-11-07 22:56:47 -0500

    Disable EE hands... completely not needed nor wanted
    I had this initially enabled because I didn't know what I was doing, honestly

--- keyboards/keebio/iris/keymaps/bendalton/config.h
+++ keyboards/keebio/iris/keymaps/bendalton/config.h
@@ -18,7 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 // #define USE_I2C
-#define EE_HANDS
+// #define EE_HANDS
 
 #undef RGBLED_NUM
 #define RGBLED_NUM 14
