5d182fcb213e85c13c12bbe692d754c19601b3e8
Mark Stosberg <mark@rideamigos.com>
2020-08-26 22:27:38 -0400

    Disable RGB lights, OLED features.

--- keyboards/crkbd/keymaps/markstos/README.md
+++ keyboards/crkbd/keymaps/markstos/README.md
@@ -1,4 +1,13 @@
 
+## RGB Matrix timeout
+
+This keymap will set an automated timeout for the RGB Matrix.
+
+After 5 minutes, the matrix will be switched off.
+
+Pressing any key re-enables the lights.
+
 ## Flashing
 
     make crkbd/rev1:markstos:dfu
+
--- keyboards/crkbd/keymaps/markstos/rules.mk
+++ keyboards/crkbd/keymaps/markstos/rules.mk
@@ -2,10 +2,10 @@
 #BOOTMAGIC_ENABLE = lite
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/glcdfont.c \
-        ./lib/rgb_state_reader.c \
-        ./lib/layer_state_reader.c \
-        ./lib/keylogger.c \
-        ./lib/mode_icon_reader.c \
+        #./lib/keylogger.c
+        #./lib/layer_state_reader.c \
+        #./lib/rgb_state_reader.c \
+        #./lib/mode_icon_reader.c \
         #./lib/logo_reader.c \
         # ./lib/host_led_state_reader.c \
         # ./lib/timelogger.c \
@@ -13,13 +13,14 @@ SRC +=  ./lib/glcdfont.c \
 # markstos: enable media keys
 EXTRAKEY_ENABLE = yes
 
-# markstos: smaller file size, litlte downsize
+# markstos: smaller file size, little down-side
 LINK_TIME_OPTIMIZATION_ENABLE = yes
 
 # markstos: for combos!
 COMBO_ENABLE = yes
 
 # markstos: enable LEDs
+# When I enable this, the left half doesn't work.
 #RGB_MATRIX_ENABLE = WS2812
 
 # markstos: try QMK's DFU
@@ -31,4 +32,4 @@ BACKLIGHT_ENABLE           = no  # Enable keyboard backlight functionality
 
 # This is for RGB *underglow*
 # https://github.com/qmk/qmk_firmware/blob/master/docs/feature_rgblight.md
-RGBLIGHT_ENABLE = yes
+RGBLIGHT_ENABLE = no
