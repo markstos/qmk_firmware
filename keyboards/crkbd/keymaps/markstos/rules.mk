1704e9459586ed4ea881aa662815c865e18923a2
Mark Stosberg <mark@rideamigos.com>
2021-05-23 21:20:06 -0400

    remove commented out OLED code.

--- keyboards/crkbd/keymaps/markstos/rules.mk
+++ keyboards/crkbd/keymaps/markstos/rules.mk
@@ -1,14 +1,5 @@
 # markstos: Attempt to enable Bootmagic
 #BOOTMAGIC_ENABLE = lite
-# If you want to change the display of OLED, you need to change here
-# SRC +=  ./lib/glcdfont.c \
-        #./lib/keylogger.c
-        #./lib/layer_state_reader.c \
-        #./lib/rgb_state_reader.c \
-        #./lib/mode_icon_reader.c \
-        #./lib/logo_reader.c \
-        # ./lib/host_led_state_reader.c \
-        # ./lib/timelogger.c \
 
 # markstos: enable media keys
 EXTRAKEY_ENABLE = yes
