443cb865d0fbf186f44004d9b45856f09a5d0808
Mark Stosberg <mark@rideamigos.com>
2022-04-28 17:40:41 -0400

    remove Esc Combo

--- keyboards/crkbd/keymaps/markstos-3x5/rules.mk
+++ keyboards/crkbd/keymaps/markstos-3x5/rules.mk
@@ -9,8 +9,8 @@ EXTRAKEY_ENABLE = yes
 # markstos: smaller file size, little down-side
 LINK_TIME_OPTIMIZATION_ENABLE = yes
 
-# markstos: for combos!
-COMBO_ENABLE = yes
+# markstos: no more combos
+COMBO_ENABLE = no
 
 # markstos
 # Backlighting is not RGB underglow or RGB matrix
